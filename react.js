c0af19df7767dee75c013429addb4e7c6f6e8372
Marc Harter <wavded@gmail.com>
2016-05-26 14:29:07 -0500

    Initial

---
+++ react.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": [ "./", "plugin:react/recommended" ],
+  "plugins": [ "react" ],
+  "rules": {
+    "react/sort-prop-types": 1,
+    "react/self-closing-comp": 1,
+    "react/wrap-multilines": 1,
+    "react/jsx-boolean-value": 1,
+    "react/jsx-closing-bracket-location": 1,
+    "react/jsx-curly-spacing": 1,
+    "react/jsx-equals-spacing": 1,
+    "react/jsx-space-before-closing": 1,
+    "react/jsx-indent-props": [1, 2],
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
+    },
+  },
+}
